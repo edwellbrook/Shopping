a99defc8dfc51a8fe8d5cac881e89ad6329baa3a
Ed Wellbrook <edwellbrook@gmail.com>
2016-11-17 10:45:13 +0000

    Extract out config code

--- server/src/cmd/shopping/main.go
+++ server/src/cmd/shopping/main.go
@@ -2,33 +2,54 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"flag"
 	"log"
 
 	"github.com/tarm/serial"
 )
 
-func main() {
-	com := flag.String("com", "", "COM port for transferring data")
+type Config struct {
+	SerialName string
+	SerialBaud int
+}
+
+func appConfig() (*Config, error) {
+	conf := &Config{}
+
+	name := flag.String("com", "", "COM port for transferring data")
 	baud := flag.Int("baud", 9600, "Baud rate for COM port")
 
 	flag.Parse()
 
-	if *com == "" {
-		log.Fatal("A COM port must be specified")
+	if *name == "" {
+		return conf, errors.New("A COM port must be specified")
 	}
 
-	sConf := &serial.Config{Name: *com, Baud: *baud}
+	conf.SerialName = *name
+	conf.SerialBaud = *baud
 
-	port, err := serial.OpenPort(sConf)
+	return conf, nil
+}
+
+func main() {
+	config, err := appConfig()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	serialConfig := &serial.Config{
+		Name: config.SerialName,
+		Baud: config.SerialBaud,
+	}
+
+	port, err := serial.OpenPort(serialConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer port.Close()
 
 	reader := bufio.NewReader(port)
-
 	for {
 		line, _, err := reader.ReadLine()
 		if err != nil {
