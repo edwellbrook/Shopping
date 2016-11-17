679365392a2b80fe9717d010be001c86a8f6753a
Ed Wellbrook <edwellbrook@gmail.com>
2016-11-17 10:30:54 +0000

    Read and print data from serial port

--- server/src/cmd/shopping/main.go
+++ server/src/cmd/shopping/main.go
@@ -1,7 +1,39 @@
 package main
 
-func main() {
-	for {
+import (
+	"bufio"
+	"flag"
+	"log"
 
+	"github.com/tarm/serial"
+)
+
+func main() {
+	com := flag.String("com", "", "COM port for transferring data")
+	baud := flag.Int("baud", 9600, "Baud rate for COM port")
+
+	flag.Parse()
+
+	if *com == "" {
+		log.Fatal("A COM port must be specified")
+	}
+
+	sConf := &serial.Config{Name: *com, Baud: *baud}
+
+	port, err := serial.OpenPort(sConf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer port.Close()
+
+	reader := bufio.NewReader(port)
+
+	for {
+		line, _, err := reader.ReadLine()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("%q", line)
 	}
 }
