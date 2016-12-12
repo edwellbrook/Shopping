73efb0ed6cb73c7dddd2bc186546763f48ef3df4
Ed Wellbrook <edwellbrook@gmail.com>
2016-12-12 02:19:35 +0000

    Move serial handling to own file

--- server/src/cmd/shopping/main.go
+++ server/src/cmd/shopping/main.go
@@ -1,25 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"log"
-	"serial_api"
-	"strings"
-	"time"
 
-	"github.com/cenkalti/backoff"
-	"github.com/tarm/serial"
 	mqtt_client "github.com/yosssi/gmq/mqtt/client"
 )
 
-type SerialResponse struct {
-	Data  []byte
-	Error error
-}
-
 var config *Config
-var serialPort *serial.Port
 var mqttClient *mqtt_client.Client
 var serialChan chan SerialResponse
 
@@ -47,100 +35,10 @@ func foundBeacon(beaconId string) {
 	log.Printf("Found beacon: %s", beaconId)
 }
 
-// func setupSerial(config Config) func() error {
-// 	return func() (err error) {
-// 		serialPort, err = serial.OpenPort(config.Serial)
-// 		if err != nil {
-// 			log.Println(err)
-// 		}
-// 		return err
-// 	}
-// }
-//
-// func connectToSerial(config Config) error {
-// 	serialBackoff := backoff.NewExponentialBackOff()
-// 	serialBackoff.MaxElapsedTime = time.Minute
-//
-// 	err := backoff.Retry(setupSerial(config), serialBackoff)
-// 	if err != nil {
-// 		return err
-// 	}
-//
-// 	return nil
-// }
-
-func processSerial(response SerialResponse) {
-	if response.Error != nil {
-		// there was an error, attempt to reconnect to hopefully resolve it
-		go setupSerial()
-		return
-	}
-
-	resp := serial_api.Parse(response.Data)
-
-	switch resp.Type {
-	case serial_api.INIT:
-		log.Println("Application starting")
-
-	case serial_api.INFO:
-		msg := strings.Join(resp.Args, " ")
-		log.Println(msg)
-
-	case serial_api.AUTH:
-		if len(resp.Args) == 1 {
-			cardId := []byte(resp.Args[0])
-			authoriseCard(cardId)
-		}
-
-	case serial_api.SCAN:
-		if len(resp.Args) == 1 {
-			foundBeacon(resp.Args[0])
-		}
-
-	default:
-		log.Printf("Application sent unhandled message: %s\n", resp.Type)
-	}
-}
-
-func setupSerial() {
-	b := backoff.NewExponentialBackOff()
-	b.MaxElapsedTime = time.Minute
-
-	err := backoff.Retry(func() (err error) {
-		println("attempting to open ting")
-
-		serialPort, err = serial.OpenPort(config.Serial)
-		if err != nil {
-			log.Printf("-> %s", err)
-		}
-		return err
-	}, b)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer serialPort.Close()
-
-	reader := bufio.NewReader(serialPort)
-
-	for {
-		line, _, err := reader.ReadLine()
-		serialChan <- SerialResponse{line, err}
-
-		if err != nil {
-			break
-		}
-	}
-}
-
 func main() {
-	var err error
+	config = mustLoadConfig()
 
-	config, err = appConfig()
-	if err != nil {
-		log.Fatal(err)
-	}
+	serialChan = make(chan SerialResponse)
 
 	// err = connectToSerial(*config)
 	// if err != nil {
@@ -152,8 +50,6 @@ func main() {
 	// if err != nil {
 	// 	log.Fatal(err)
 	// }
-
-	serialChan = make(chan SerialResponse)
 
 	go setupSerial()
 
