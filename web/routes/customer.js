9fc60aaf215356276c213bdfc8e3d767eec884a8
Ed Wellbrook <edwellbrook@gmail.com>
2016-12-13 12:59:06 +0000

    Add customer card registration

---
+++ web/routes/customer.js
@@ -0,0 +1,33 @@
+var express = require('express')
+
+module.exports = function(database) {
+  var router = express.Router()
+
+  router.get('/', function(req, res, next) {
+    res.send('respond with a resource')
+  })
+
+  router.get('/register', function(req, res, next) {
+    res.render('customer/register', {})
+  })
+
+  router.post('/register', function(req, res, next) {
+    let card = req.body.card_id
+    if (card == null) {
+      let err = new Error('Invalid Card ID')
+      err.status = 400
+
+      return next(err)
+    }
+
+    database.query('INSERT INTO cards VALUES ($1, $2)', [card, -1], function(err) {
+      if (err != null) {
+        return next(err)
+      }
+
+      res.send('nice one bruvva')
+    })
+  })
+
+  return router
+}
