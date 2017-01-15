e135e8f97f450e4e5b68df764ea1dd430b7bc0c7
Ed Wellbrook <edwellbrook@gmail.com>
2017-01-15 23:17:40 +0000

    Update sql

--- db-setup.sql
+++ db-setup.sql
@@ -6,3 +6,11 @@ CREATE TABLE IF NOT EXISTS cards (
 
   CONSTRAINT cards_id_key PRIMARY KEY (id)
 );
+
+-- beacons mapped to location names
+CREATE TABLE IF NOT EXISTS locations (
+  id          varchar(16)                    NOT NULL,
+  name        varchar(60)                    NOT NULL,
+
+  CONSTRAINT locations_id_key PRIMARY KEY (id)
+);
